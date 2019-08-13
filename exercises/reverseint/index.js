b8f9f88405d1c71578dc658643dd700c55175336
Dil Sombayeva <dilyasom@gmail.com>
2019-08-13 15:23:30 -0700

    reverseInt

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,35 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+ // Number doesn't work here
+ let str =
+  parseInt(
+   n
+    .toString()
+    .split('')
+    .reverse()
+    .join('')
+  ) * Math.sign(n);
+
+ return str;
+}
+// function reverseInt(n) {
+//  let str = n.toString();
+//  if (n < 0) {
+//   str = str
+//    .slice(1)
+//    .split('')
+//    .reverse()
+//    .join('');
+//   return Number(-str);
+//  } else {
+//   str = str
+//    .split('')
+//    .reverse()
+//    .join('');
+//   return Number(str);
+//  }
+// }
 
 module.exports = reverseInt;
