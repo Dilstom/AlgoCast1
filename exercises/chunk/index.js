e6758cd1e4124b5f99c566344ef13a9985cda25e
Dil Sombayeva <dilyasom@gmail.com>
2019-08-14 11:09:09 -0700

    chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,19 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+ let newArr = [];
+
+ let chunk = [];
+ while (array.length > 0) {
+  chunk = array.slice(0, size);
+  newArr.push(chunk);
+  array.splice(0, size);
+  chunk = [];
+ }
+ //   console.log(chunk)
+ //   console.log('newArr', newArr)
+ return newArr;
+}
 
 module.exports = chunk;
