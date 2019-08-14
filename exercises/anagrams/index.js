60c5ff3a9baa4591c2a1b54f4ff12dbeca7d7f6d
Dil Sombayeva <dilyasom@gmail.com>
2019-08-14 13:14:01 -0700

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,63 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// function anagrams(stringA, stringB) {
+//  const strA = stringA.replace(/[^\w]/g, '').toLowerCase();
+//  const strB = stringB.replace(/[^\w]/g, '').toLowerCase();
+//  const newObj1 = {};
+//  const newObj2 = {};
+
+//  function iter(str, obj) {
+//   for (let el of str) {
+//    obj[el] = obj[el] + 1 || 1;
+//   }
+//   //   console.log(obj);
+//  }
+
+//  iter(strA, newObj1);
+//  iter(strB, newObj2);
+
+//  if (Object.keys(newObj1).length !== Object.keys(newObj2).length) {
+//   return false;
+//  }
+
+//  console.log(newObj2['r']);
+//  for (let i in newObj1) {
+//   // console.log(i) // key
+//   if (newObj1[i] !== newObj2[i]) {
+//    return false;
+//   }
+//  }
+//  return true;
+// }
 
 module.exports = anagrams;
+
+// orig sol:
+
+function anagrams(stringA, stringB) {
+ const aCharMap = buildCharMap(stringA);
+ const bCharMap = buildCharMap(stringB);
+
+ if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+  return false;
+ }
+
+ for (let char in aCharMap) {
+  if (aCharMap[char] !== bCharMap[char]) {
+   return false;
+  }
+ }
+
+ return true;
+}
+
+function buildCharMap(str) {
+ const charMap = {};
+
+ for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+  charMap[char] = charMap[char] + 1 || 1;
+ }
+
+ return charMap;
+}
