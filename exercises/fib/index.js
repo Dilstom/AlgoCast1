f86567c614067ad2c4841aedf94972da712cb5c4
Dil Sombayeva <dilyasom@gmail.com>
2019-08-18 10:23:14 -0700

    fib - recursion

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,14 @@
 // Example:
 //   fib(4) === 3
 
+
+// using recursion
+// function fib(n) {
+//  if (n < 2) return n;
+
+//  return fib(n - 2) + fib(n - 1);
+// }
+
 // using iteration
 // function fib(n) {
 //  const newArr = [0, 1];
