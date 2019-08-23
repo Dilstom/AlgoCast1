77e3e279bd3ab725029c255d181c88974689a877
Dil Sombayeva <dilyasom@gmail.com>
2019-08-22 18:23:53 -0700

    tree - levelWidth

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,22 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+ let counter = [0];
+ const s = 'stopper';
+ let arr = [root, s];
+ while (arr.length > 1) {
+  let node = arr.shift();
+
+  if (node === s) {
+   arr.push(s);
+   counter.push(0);
+  } else {
+   arr.push(...node.children);
+   counter[counter.length - 1]++;
+  }
+ }
+ return counter;
+}
 
 module.exports = levelWidth;
